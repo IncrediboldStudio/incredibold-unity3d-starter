9ab2f54f0dab8750bbabd95b0ede61eed33499ee
Nicolas-Bisson <nicolasbisson14@gmail.com>
2023-07-07 00:04:59 -0400

    UIToolkit menu start

--- Assets/PROJECT_NAME/UI/Menu/MainMenu/MainMenu.cs
+++ Assets/PROJECT_NAME/UI/Menu/MainMenu/MainMenu.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] List<GameObject> submenus;
+    [SerializeField] UIDocument uiDocument;
 
-    public void StartGame()
+    private VisualElement root;
+
+    public void Start()
+    {
+        root = uiDocument.rootVisualElement;
+
+        var button = root.Q<Button>("Play");
+        button.RegisterCallback<ClickEvent>(StartGame);
+    }
+
+    private void StartGame(ClickEvent evt = null)
     {
         LoadingManager.Instance.ChangeScene("MainScene");
     }
